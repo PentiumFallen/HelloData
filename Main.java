1970d027a18c128a3c143735ecf5793054692b25
Pentium Fallen <raul.vargas@upr.edu>
2018-02-24 17:40:18 -0400

    Useless feature

--- Main.java
+++ Main.java
@@ -6,7 +6,6 @@ public class Main{
 		System.out.print("Enter a radius to calculate its area: ");
 		Scanner input = new Scanner(System.in);
 		Double r = input.nextDouble();
-		System.out.println("The area of a circle with radius "+r+" is "+(3.14*r));
-		System.out.println("Half of that is "+(3.14*r/2));
+		System.out.println("The area of a circle with radius "+r+" is "+(3.14*r*r));
 	}
 }
