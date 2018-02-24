a9b51f4b37ade9b6886a296674036186e62aba14
Pentium Fallen <raul.vargas@upr.edu>
2018-02-24 17:30:32 -0400

    Area calc

--- Main.java
+++ Main.java
@@ -1,5 +1,11 @@
+import java.util.Scanner;
+
 public class Main{
 	public static void main(String args[]) {
 		System.out.println("Hello Data");
+		System.out.print("Enter a radius to calculate its area: ");
+		Scanner input = new Scanner(System.in);
+		Double r = input.nextDouble();
+		System.out.println("The area of a circle with radius "+r+" is: "+(3.14*r));
 	}
 }
