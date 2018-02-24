424387396d47bb282ef5a970b522c43fd3c0a6af
Pentium Fallen <raul.vargas@upr.edu>
2018-02-24 17:35:30 -0400

    Half area feature

--- Main.java
+++ Main.java
@@ -6,6 +6,7 @@ public class Main{
 		System.out.print("Enter a radius to calculate its area: ");
 		Scanner input = new Scanner(System.in);
 		Double r = input.nextDouble();
-		System.out.println("The area of a circle with radius "+r+" is: "+(3.14*r));
+		System.out.println("The area of a circle with radius "+r+" is "+(3.14*r));
+		System.out.println("Half of that is "+(3.14*r/2));
 	}
 }
